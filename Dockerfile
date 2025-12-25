a82f84b64cfab48eb018e52ffabf280a550f2341
OumaymaBrd <oumaymabramid@gmail.com>
2025-12-25 01:07:28 +0100

    Dockeriser l application Spring + Postgres

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,21 @@
 FROM eclipse-temurin:17-jdk-jammy AS build
 WORKDIR /app
+
+RUN apt-get update && apt-get install -y dos2unix
+
 COPY mvnw .
-COPY mvnw.cmd .
 COPY .mvn .mvn
 COPY pom.xml .
+
+RUN dos2unix mvnw && chmod +x mvnw
+
 RUN ./mvnw dependency:go-offline
 COPY src ./src
 
 RUN ./mvnw clean package -Dmaven.test.skip=true
 
-FROM eclipse-temurin:17-jdk-jammy
+FROM eclipse-temurin:17-jre-jammy
 WORKDIR /app
 COPY --from=build /app/target/*.jar app.jar
-EXPOSE 8080
+EXPOSE 9089
 ENTRYPOINT ["java", "-jar", "app.jar"]
--- src/main/java/org/example/smartspring/SmartSpringApplication.java
+++ src/main/java/org/example/smartspring/SmartSpringApplication.java
@@ -7,10 +7,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SmartSpringApplication {
     public static void main(String[] args) {
+        // Cette configuration ne plante pas si le .env est absent (cas Docker)
+        Dotenv dotenv = Dotenv.configure()
+                .ignoreIfMissing()
+                .load();
 
-        Dotenv dotenv = Dotenv.load();
-        System.setProperty("GMAIL_USERNAME", dotenv.get("GMAIL_USERNAME"));
-        System.setProperty("GMAIL_APP_PASSWORD", dotenv.get("GMAIL_APP_PASSWORD"));
+        // Récupération avec priorité : .env > Variable Système
+        String gmailUser = dotenv.get("GMAIL_USERNAME") != null ?
+                dotenv.get("GMAIL_USERNAME") : System.getenv("GMAIL_USERNAME");
+        String gmailPass = dotenv.get("GMAIL_APP_PASSWORD") != null ?
+                dotenv.get("GMAIL_APP_PASSWORD") : System.getenv("GMAIL_APP_PASSWORD");
+
+        if (gmailUser != null) System.setProperty("GMAIL_USERNAME", gmailUser);
+        if (gmailPass != null) System.setProperty("GMAIL_APP_PASSWORD", gmailPass);
 
         SpringApplication.run(SmartSpringApplication.class, args);
     }
